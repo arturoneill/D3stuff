489820759e1972641d8f3801d69d1e916c1e26b9
arturoneill <mobius0919@gmail.com>
2018-09-06 14:04:14 -0400

    Update simplebar.js

--- JS/simplebar.js
+++ JS/simplebar.js
@@ -1,3 +1,18 @@
 var chartdata = [];
 for( var i=0; i<10; i++) {
-var mydata=
+var mydata = Math.random()*300;
+  chartdata.push(mydata);
+    }
+
+d3.select("body").selectAll("div")
+.data(chartdata)
+.enter()
+.append("div")
+.attr("class", "bar")
+.style("height", function(d){
+  return d + "px";
+});
+
+
+
+
