df04710b141344c4d72423f9f3011a9847e9c934
arturoneill <mobius0919@gmail.com>
2018-09-08 12:04:22 -0400

    Update usviz.js

--- JS/usviz.js
+++ JS/usviz.js
@@ -5,6 +5,16 @@ var	margin = {top: 30, right: 20, bottom: 30, left: 50},
 	width = 1200 - margin.left - margin.right,
 	height = 600 - margin.top - margin.bottom;
 	
+
+	
+var pop_domain = [0, 10000, 50000, 100000, 200000, 500000, 1000000, 2000000]
+var pop_color = d3.scaleThreshold()
+    .domain(pop_domain)
+    .range(d3.schemeGreens[7]);	
+	
+	
+var popData = d3.map();
+	
 //define svg variable	
 	
 var svg = d3.select("#usmap")
@@ -18,6 +28,14 @@ var svg = d3.select("#usmap")
 //read in world.topojson
 d3.queue()
 	.defer(d3.json, "uscounty.topojson")
+	.defer(d3.csv, "data/UScountypop.csv", function(d) {
+        if (d.respop72017 == '-') {
+            povertyData.set(d.id2, 0);
+        } else {
+            povertyData.set(d.id2, +d.respop72017); 
+        }
+        
+    })
 	.await(ready)
  
 //define projection
@@ -31,7 +49,7 @@ d3.queue()
  
   
    //function that feeds data to geopath so it can draw 
-function ready (error, data) {
+function ready (error, data, popData) {
 	console.log(data)
 
 	 //feature "obects.xxxx" has to include xxxx from actual topojson file 
@@ -44,6 +62,13 @@ function ready (error, data) {
 	.enter().append("path")
 	.attr("class", "county")
 	.attr("d", path)
+	  .attr("fill", function(d) { 
+            var value = popData.get(d.objects.id);
+            return (value != 0 ? pop_color(value) : "lightblue");  
+
+        })
+        
+	
 	//add class 'selected'
 	.on('mouseover', function(d) {
 		d3.select(this).classed("selected", true)
