1dfa2163db1c4d663c8a7fffe6e92bfa708082c1
arturoneill <mobius0919@gmail.com>
2018-09-10 03:01:11 -0400

    Update uspopviz.js

--- JS/uspopviz.js
+++ JS/uspopviz.js
@@ -37,7 +37,7 @@ var popData = d3.map();
 d3.queue()
     .defer(d3.json, "data/uscounty.topojson")
     .defer(d3.csv, "data/UScountypop.csv", function(d) { 
-        if (d.respop72014) {
+        if (isNaN(d.respop72014)) {
             popData.set(d.id2, 0); 
         } else {
             popData.set(d.id2, +d.respop72014); 
